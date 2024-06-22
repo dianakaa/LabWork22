3b9ee6ba4996ac06fe1c239e655ec9494417d458
ki1lmyself <sofiamiklzkova@gmail.com>
2024-06-22 17:59:46 +0300

    Добавьте файлы проекта.

---
+++ LabWork22/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace LabWork22
+{
+    internal class Program
+    {
+        static void GetPower(double a, int x)
+        {
+            double result = 1;
+            for (int i = 0; i < x; i++)
+            {
+                result *= a;
+            }
+
+            if (x < 0)
+            {
+                for (int i = 0; i < -x; i++)
+                {
+                    result *= a;
+                }
+                result = 1 / result;
+            }
+
+            Console.WriteLine($"{a}^{x}={result}");
+        }
+
+        static async Task Main(string[] args)
+        {
+            await Task.Run(() => GetPower(2, -3));
+            await Task.Run(() => GetPower(3, 4));
+            await Task.Run(() => GetPower(4, 5));
+
+            Task task1 = Task.Run(() => GetPower(2, 3));
+            Task task2 = Task.Run(() => GetPower(3, 4));
+            Task task3 = Task.Run(() => GetPower(4, 5));
+            await Task.WhenAll(task1, task2, task3);
+        }
+
+
+    }
+}
---
+++ Task3/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Threading.Tasks;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        CalculatePowerAsync(2, 3);
+        CalculatePowerAsync(3, 4);
+        CalculatePowerAsync(5, 2);
+    }
+    static double GetPower(double a, int x)
+    {
+        double result = 1;
+        for (int i = 0; i < x; i++)
+        {
+           return result *= a;
+        }
+        return result;
+    }
+  
+    static async void CalculatePowerAsync(int a, int x)
+    {
+        double result = await PowerAsync(a, x);
+        Console.WriteLine($"{a}^{x} = {result}");
+    }
+
+    static Task<double> PowerAsync(int a, int x)
+    {
+        return Task.Run(() => GetPower(a, x));
+    }
+
+    static async Task Expression(int a1, int x1, int a2, int x2, int a3, int x3, int a4, int x4)
+    {
+        double result1 = await PowerAsync(1, 2);
+        double result2 = await PowerAsync(3, 4);
+        double result3 = await PowerAsync(5, 6);
+        double result4 = await PowerAsync(7, 8);
+
+        if (result3 - result4 != 0)
+        {
+            double finalResult = (double)(result1 + result2) / (result3 - result4);
+            Console.WriteLine($"{a1}^{x1} + {a2}^{x2}) / ({a3}^{x3} - {a4}^{x4}) = {finalResult}");
+        }
+        else
+        {
+            Console.WriteLine("Делить на 0 нельзя");
+        }
+    }
+}
